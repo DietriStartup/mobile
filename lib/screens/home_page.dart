8a49dab6956a610525a689a36963d7b538d03b6c
Akinlade Adesina <66315685+ideas2web@users.noreply.github.com>
2022-07-06 22:00:46 +0100

    reusablecards-quote-food-breakfast

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,11 @@
+import 'package:dietri/components/reueable_food_card.dart';
+import 'package:dietri/components/reuseable_quote_card.dart';
+import 'package:dietri/constants/colors.dart';
 import 'package:dietri/services/auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+
+import '../components/reuseable_breakfast_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -9,10 +14,16 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     final auth = context.read<AuthBase>();
     return Scaffold(
+      backgroundColor: kWhiteColor,
       body: SafeArea(
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+        //mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          ReusableQuoteCard(),
+          SizedBox(height: 20,),
+          ReuseableBreakfastCard(),
+          SizedBox(height: 20,),
+          ReuseableFoodCard(),
           const Center(
             child: Text('youre welcome'),
           ),
